dabd7c287e9c31a1bbdcf92acc33207f1ae4c210
juancarlos301 <juancarlos.31.2002@gmail.com>
2023-09-06 14:58:38 -0500

    add movies changes

--- src/components/MoviesHome/MoviesHome.jsx
+++ src/components/MoviesHome/MoviesHome.jsx
@@ -1,11 +1,6 @@
-import React, { useEffect, useState } from "react";
 import { MovieBox } from "./MovieBox";
-import axios from "axios";
-import { MainContainer } from "./MoviesHomeElements";
 
-//const API_KEY = process.env.REACT_APP_API_KEY;
-//const API_BASE = process.env.REACT_APP_API_URL;
-//const API_TOP_RATED_MOVIES_URL = `${API_BASE}movie/top_rated?api_key=${API_KEY}&language=es-US&page=1`;
+import { MainContainer } from "./MoviesHomeElements";
 
 export const MoviesHome = ({ movies }) => {
   return (
