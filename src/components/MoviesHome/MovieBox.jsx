78b804cd005e74684102fe760cc350679081bc0a
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-09-11 16:35:22 -0500

    se agrega nuevo metodo para peliculas favoritas

--- src/components/MoviesHome/MovieBox.jsx
+++ src/components/MoviesHome/MovieBox.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import {
   Card,
-  // StyleButton,
   StylePlayCircleOutlineIcon,
   StyledModal,
   StyledPaper,
@@ -11,6 +10,7 @@ import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import Stack from "@mui/material/Stack";
+import Alert from "@mui/material/Alert";
 
 const API_IMG = "https://image.tmdb.org/t/p/w500/";
 
@@ -22,6 +22,9 @@ export const MovieBox = ({
   overview,
 }) => {
   const [open, setOpen] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [successAlert, setSuccessAlert] = useState(false);
+  const [alreadyInFavoritesAlert, setAlreadyInFavoritesAlert] = useState(false);
 
   const handleOpen = () => {
     setOpen(true);
@@ -31,9 +34,34 @@ export const MovieBox = ({
     setOpen(false);
   };
 
+  const handleToggleFavorite = () => {
+    // Comprueba si la película ya está en favoritos
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+    // Comprueba si la película actual ya está en favoritos comparando el título
+    const isAlreadyFavorite = favorites.some((movie) => movie.title === title);
+
+    if (isAlreadyFavorite) {
+      // Si la película ya es favorita, muestra una alerta indicando que ya está en favoritos
+      setSuccessAlert(false); // Oculta la alerta de éxito (si estaba mostrándose)
+      setAlreadyInFavoritesAlert(true); // Muestra la alerta de "ya en favoritos"
+    } else {
+      // Si la película no es favorita, agrégala a favoritos
+      const newFavorite = { title, poster_path, vote_average, release_date };
+      localStorage.setItem(
+        "favorites",
+        JSON.stringify([...favorites, newFavorite])
+      );
+      setAlreadyInFavoritesAlert(false); // Oculta la alerta de "ya en favoritos" (si estaba mostrándose)
+      setSuccessAlert(true); // Muestra la alerta de éxito
+
+      // Cambia el estado para indicar que ahora la película es favorita
+      setIsFavorite(true);
+    }
+  };
+
   return (
     <>
-      
       <Card>
         <img
           src={API_IMG + poster_path}
@@ -44,7 +72,6 @@ export const MovieBox = ({
           onClick={handleOpen}
           sx={{ fontSize: 70 }}
         />
-        {/* <StyleButton>{release_date}</StyleButton> */}
 
         <Stack
           sx={{
@@ -79,17 +106,36 @@ export const MovieBox = ({
                   variant="contained"
                   color="error"
                   startIcon={<FavoriteIcon />}
+                  onClick={handleToggleFavorite}
                   sx={{
                     marginTop: 2,
                     width: "150px",
+                    backgroundColor: isFavorite ? "red" : "", // Cambia el color del botón si es favorito
                   }}
                 >
-                  Añadir a Favoritas
+                  {isFavorite ? "Eliminar de Favoritas" : "Añadir a Favoritas"}
                 </Button>
               </Stack>
             </StyledPaper>
           </StyledModal>
         </Stack>
+        {successAlert && (
+          <Alert
+            onClose={() => setSuccessAlert(false)}
+            severity="success"
+          >
+            Película agregada a favoritas con éxito
+          </Alert>
+        )}
+
+        {alreadyInFavoritesAlert && (
+          <Alert
+            onClose={() => setAlreadyInFavoritesAlert(false)}
+            severity="info"
+          >
+            Esta película ya está en tus favoritas
+          </Alert>
+        )}
       </Card>
     </>
   );
