93f9fec254d3ca882643409e8a876a0df6621406
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-09-11 17:04:28 -0500

    cards implementadas

--- src/components/FavoriteMovies/FavoriteMovies.jsx
+++ src/components/FavoriteMovies/FavoriteMovies.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import {
   Typography,
-  List,
-  ListItem,
-  ListItemText,
+  Card,
+  CardContent,
+  CardMedia,
   Button,
   Alert,
 } from "@mui/material";
 
+const API_IMG = "https://image.tmdb.org/t/p/w500/";
+
 export const FavoriteMovies = () => {
-    const [favorites, setFavorites] = useState([]);
-    const [successAlert, setSuccessAlert] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+  const [successAlert, setSuccessAlert] = useState(false);
 
   useEffect(() => {
     const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
@@ -25,12 +27,37 @@ export const FavoriteMovies = () => {
   };
 
   return (
-    <div>
-      <Typography variant="h2">Películas Favoritas</Typography>
-      <List>
-        {favorites.map((movie) => (
-          <ListItem key={movie.title}>
-            <ListItemText primary={movie.title} />
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        maxHeight: "100%",
+        flexDirection: "column",
+        backgroundColor: "#080f28",
+        maxwidth: "100%",
+      }}
+    >
+      <Typography variant="h2" sx={{color:"white", mt:"15px"}}>Películas Favoritas</Typography>
+      {favorites.map((movie) => (
+        <Card
+          key={movie.title}
+          style={{ margin: "16px", maxWidth: "400px" }}
+        >
+          <CardMedia
+            component="img"
+            height="200"
+            src={API_IMG + movie.poster_path}
+            alt={movie.title}
+          />
+          <CardContent>
+            <Typography
+              variant="h5"
+              component="div"
+            >
+              {movie.title}
+            </Typography>
             <Button
               variant="contained"
               color="error"
@@ -38,19 +65,18 @@ export const FavoriteMovies = () => {
             >
               Eliminar
             </Button>
-          </ListItem>
-        ))}
-      </List>
+          </CardContent>
+        </Card>
+      ))}
       {successAlert && (
         <Alert
           onClose={() => setSuccessAlert(false)}
           severity="success"
+          style={{ marginTop: "16px" }}
         >
-          Película eliminada de Favoritas con exito
+          Película eliminada de Favoritas con éxito
         </Alert>
       )}
     </div>
   );
 };
-
-
