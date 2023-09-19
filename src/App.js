4a3b686e121e13a91dc0d4f6f24d05810ff2b910
juancarlos301 <juancarlos.31.2002@gmail.com>
2023-09-19 14:01:11 -0500

    add changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { MovieProvider } from "./MovieContext";
 import { Home } from "./Pages/Home";
-import { SearchAppBar } from "./components/navbar/Appbar";
+import { SearchAppBar } from "./components/NavBar/Appbar";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { Favoritas } from "./Pages/Favoritas";
@@ -11,18 +11,9 @@ function App() {
       <div style={{ backgroundColor: "#080f28", minHeight: "100vh" }}>
         <SearchAppBar />
         <Routes>
-          <Route
-            path="/"
-            element={<Navigate to="/Inicio" />}
-          />
-          <Route
-            path="/Inicio"
-            element={<Home />}
-          />
-          <Route
-            path="/Favoritas"
-            element={<Favoritas />}
-          />
+          <Route path="/" element={<Navigate to="/Inicio" />} />
+          <Route path="/Inicio" element={<Home />} />
+          <Route path="/Favoritas" element={<Favoritas />} />
         </Routes>
       </div>
     </MovieProvider>
